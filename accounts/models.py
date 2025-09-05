f79423ac18741c990a4ff0672c32eaaa14813fc5
Poojan Anghan <anghanarpit@gmail.com>
2025-09-05 18:31:10 +0530

    custom user model and role based access added

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,41 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-from 
-# Create your models here.
+from django.db import models
 
-
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     ROLE_CHOICES = (
-        ("admin", "Admin"),
-        ("teacher", "Teacher"),
         ("student", "Student"),
+        ("teacher", "Teacher"),
+        ("admin", "Admin"),
+        ("super admin", "Superadmin")
     )
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="student")
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+
+class BaseProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {self.user.username}"
+
+
+class StudentProfile(BaseProfile):
+    roll = models.CharField(max_length=20, unique=True)
+    course = models.CharField(max_length=100)
+    year = models.IntegerField()
+    section = models.CharField(max_length=10)
+
+
+class TeacherProfile(BaseProfile):
+    subject = models.CharField(max_length=50)
+    department = models.CharField(max_length=100)
+    designation = models.CharField(max_length=50)
+
+
+class AdminProfile(BaseProfile):
+    office = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
--- adv_DRF/settings.py
+++ adv_DRF/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = "accounts.CustomUser"
+
 
 
 REST_FRAMEWORK = {
